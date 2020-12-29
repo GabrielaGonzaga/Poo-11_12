884e2ccd160f3c0c65bc3cdf4cd24c756ef0657d
GabrielaGonzaga <73176744+GabrielaGonzaga@users.noreply.github.com>
2020-12-29 18:58:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using POO_1112.Classes;
+
+namespace POO_1112
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //instanciamos nossa claase de Funcionário
+            Funcionario fun = new Funcionario();
+            fun.Mostrar();
+            fun.Mostrar(" Gabriela");
+            fun.Mostrar(3);
+
+            Console.Beep();
+        }
+    }
+}
